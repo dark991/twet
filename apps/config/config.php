4577796f30ff2ea0b8121d80ea0ace02757bc5ea
dark991 <dark991@yandex.ru>
2016-09-12 21:23:42 +0300

    AuthComponent

--- apps/config/config.php
+++ apps/config/config.php
@@ -13,19 +13,31 @@ return new \Phalcon\Config(
             'main' => $host,
         ],
         'twitch' => [
-            'client_id' => '',
+            'client_id' => 'ru0xspiy28g46max3mqdxuywpc4za16',
             'client_secret' => '1',
             'redirect_uri' => $host . '/twitch/callback',
         ],
         'crypt' => [
-            'key' => '',
+            'key' => 'd~aX%}x4<*ZxDX_vL%Hn',
         ],
         'mysql' => [
             'host' => 'virtualbox.vm',
-            'username' => '',
-            'password' => '',
+            'username' => 'twet_user',
+            'password' => 'sandMAX.1',
             'dbname' => 'twet',
             'persistent' => 'false',
         ],
     ]
 );
+
+//
+// Twet api Client ID (twitch)
+// ru0xspiy28g46max3mqdxuywpc4za16
+//
+
+/// alex_lifar stream key
+/// live_113272096_2AIjlE0FQv2nEKSv4KR6mg83rVzzTf
+
+// mysql----
+// twet_user
+// sandMAX.1
